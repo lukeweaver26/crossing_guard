cbe486b28d163c509eead8a4617fe84377748684
Luke Weaver <ljweaver@umich.edu>
2025-07-26 20:01:30 -0500

    Working graphics demo

---
+++ graphics_engine/interface/graphics_engine.hpp
@@ -0,0 +1,9 @@
+#ifndef GRAPHICS_ENGINE_HPP
+#define GRAPHICS_ENGINE_HPP
+
+class GraphicsEngine {
+    public:
+        int start();
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <traffic_engine.hpp>
+#include <graphics_engine.hpp>
 
 int main ()
 {
 	std::cout << "Crossing Guard!" << std::endl;
 
+	GraphicsEngine graphics;
 	TrafficEngine engine;
+	graphics.start();
 	engine.loop();
+
+
 }
